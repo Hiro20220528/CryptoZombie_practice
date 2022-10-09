59493aeda874216cbab86d4b9cda58dde5ee72c7
Okami Hiro <yy20soccer@gmail.com>
2022-10-09 21:24:19 +0900

    feedAndMultiply

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -5,7 +5,9 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
-    function feedAndMultiply (uint _zombieId, uint _targetDna) {
-        
+    function feedAndMultiply (uint _zombieId, uint _targetDna)  public {
+        // オーナーのみがゾンビに餌をあげれる
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
     }
 }
